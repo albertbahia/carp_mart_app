b26931eff7c93d7eae14cd96eed685fe110311bb
albertbahia <albert.bahia1@gmail.com>
2017-07-03 14:09:34 -0400

    created seed data for initial inventory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Create Item seeds with name, picture_url, description, quantity_left
 
+item_list = [
+	["alarms", "http://www.placeholder.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget ex et elit feugiat varius non sit amet tellus. Maecenas.", 10],
+	["bands", "http://www.placeholder.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget ex et elit feugiat varius non sit amet tellus. Maecenas.", 20],
+	["sleeves", "http://www.placeholder.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget ex et elit feugiat varius non sit amet tellus. Maecenas.", 15]
+]
+
+item_list.each do |name, picture_url, description, quantity_left| 
+	Item.create(name: name, picture_url: picture_url, description: decription, quantity_left: quantity_left)
+end
