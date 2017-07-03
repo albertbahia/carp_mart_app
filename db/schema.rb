2a622ffad7624de49f884178dd9ab9dbde779342
albertbahia <albert.bahia1@gmail.com>
2017-07-03 00:29:05 -0400

    created the items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170703042425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "picture_url"
+    t.string "description"
+    t.integer "quantity_left"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
